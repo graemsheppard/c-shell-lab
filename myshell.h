f190ac4c0d7ea93494e9f577445c6e79c68fda95
graemsheppard <graemsheppard@gmail.com>
2021-02-12 18:37:50 -0500

    basic commands

---
+++ myshell.h
@@ -0,0 +1,17 @@
+/*
+ * MyShell Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2021, Nicholas Lang, Graem Sheppard
+ * All rights reserved.
+ *
+ */
+#ifndef MYSHELL_H_
+#define MYSHELL_H_
+
+// Include your relevant functions declarations here they must start with the
+// extern keyword such as in the following example:
+// extern void display_help(void);
+extern void split_string(char *buffer, char ***tokens_ptr, int *tokens_size);
+
+#endif /* MYSHELL_H_ */
+#define BUFFER_LEN 256
